6cc3f2f733992e720457b722f83cafb6d4755bf9
Misza <misza@misza.net>
2018-12-12 11:21:46 +0000

    Refactor windows into their dedicated window "manager"

--- muddy.py
+++ muddy.py
@@ -9,6 +9,7 @@ import curses.ascii as asc
 
 from twisted.internet import reactor
 
+from muddylib.screen import MudScreen
 from muddylib.telnet import MudProtocol, MudClientFactory, ConnectionKeeper
 from muddylib.windows import BufferedTextWindow, InputWindow
 
@@ -22,18 +23,8 @@ class MudWindowSession:
         for c in range(16):
             curses.init_pair(c+1, c, curses.COLOR_BLACK)
 
-        y, x = screen.getmaxyx()
-
         self.screen = screen
-        self.screen.keypad(True)
-        self.screen.scrollok(False)
-        
-        x_split = x * 2 // 3
-
-        self.main_window = BufferedTextWindow('MainWindow', y-4, x_split-2, 1, 1)
-        self.chat_window = BufferedTextWindow('ChatWindow', y-4, x-x_split-2, 1, x_split+1)
-
-        self.input = InputWindow('InputWindow', x-2, y-2, 1)
+        self.mud_screen = MudScreen(screen)
         
         self.connection_keeper = ConnectionKeeper()
         pub.subscribe(self._route_incoming_text, 'Core.telnet_received')
@@ -49,38 +40,12 @@ class MudWindowSession:
         r_thread = threading.Thread(target=rrun)
         r_thread.start()
         
-        self.refresh_all()
+        self.mud_screen.refresh_all()
         while self.app_running:
             key = self.screen.getch()
             self._key_handler(key)
 
         r_thread.join()
-
-    def refresh_all(self):
-        y, x = self.screen.getmaxyx()
-        self.screen.clear()
-        
-        x_split = x * 2 // 3
-
-        # Horizontal lines
-        for x_b in range(x):
-            self.screen.addch(0, x_b, '#')
-            self.screen.addch(y-3, x_b, '#')
-            try:
-                self.screen.addch(y-1, x_b, '#')
-            except:
-                pass
-
-        # Vertical lines
-        for y_b in range(1, y-1):
-            self.screen.addch(y_b, 0, '#')
-            self.screen.addch(y_b, x_split-1, '#')
-            self.screen.addch(y_b, x-1, '#')
-
-        self.screen.refresh()
-        self.main_window.resize(y-4, x_split-2, 1, 1)
-        self.chat_window.resize(y-4, x-x_split-2, 1, x_split+1)
-        self.input.resize(x-2, y-2, 1)
 
     def write_to_main_window(self, text):
         pub.sendMessage('MainWindow.add_text', text=text)
@@ -102,7 +67,7 @@ class MudWindowSession:
 
     def _key_handler(self, key):
         if key == curses.KEY_RESIZE:
-            self.refresh_all()
+            self.mud_screen.refresh_all()
         elif key == asc.ESC:
             key = self.screen.getch()
             self._escape_key_handler(key)
---
+++ muddylib/screen.py
@@ -0,0 +1,42 @@
+from muddylib.windows import BufferedTextWindow, InputWindow
+
+
+class MudScreen:
+    def __init__(self, screen):
+        self.screen = screen
+        self.screen.keypad(True)
+        self.screen.scrollok(False)
+        
+        y, x = screen.getmaxyx()
+        x_split = x * 2 // 3
+
+        self.main_window = BufferedTextWindow('MainWindow', 1, 1, 1, 1)
+        self.chat_window = BufferedTextWindow('ChatWindow', 1, 1, 1, 1)
+
+        self.input = InputWindow('InputWindow', 1, 1, 1)
+
+    def refresh_all(self):
+        self.screen.clear()
+
+        y, x = self.screen.getmaxyx()
+        x_split = x * 2 // 3
+
+        # Horizontal lines
+        for x_b in range(x):
+            self.screen.addch(0, x_b, '#')
+            self.screen.addch(y-3, x_b, '#')
+            try:
+                self.screen.addch(y-1, x_b, '#')
+            except:
+                pass
+
+        # Vertical lines
+        for y_b in range(1, y-1):
+            self.screen.addch(y_b, 0, '#')
+            self.screen.addch(y_b, x_split-1, '#')
+            self.screen.addch(y_b, x-1, '#')
+
+        self.screen.refresh()
+        self.main_window.resize(y-4, x_split-2, 1, 1)
+        self.chat_window.resize(y-4, x-x_split-2, 1, x_split+1)
+        self.input.resize(x-2, y-2, 1)
