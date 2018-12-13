10231c08d87c22468758575039cfde484ecb2864
Misza <misza@misza.net>
2018-12-13 10:32:26 +0000

    Regex as class field, extend to {tell} tag as well

--- plugins/chat_router.py
+++ plugins/chat_router.py
@@ -5,10 +5,11 @@ from pubsub import pub
 from muddylib.plugins import IncomingTextHandler
 
 class ChatRouterPlugin:
+    chat_rx = re.compile(r'^(\{chan ch=(?P<chan>.*?)\}|\{say\}|\{tell\})(?P<text>.*)$')
+    
     @IncomingTextHandler
     def handle(self, line):
-        chat_rx = re.compile(r'^(\{chan ch=(?P<chan>.*?)\}|\{say\})(?P<text>.*)$')
-        chat_m = chat_rx.search(line)
+        chat_m = self.chat_rx.search(line)
         if chat_m:
             pub.sendMessage('ChatWindow.add_text', text=chat_m['text'])
             return True
